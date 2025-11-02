a851dcb6ba3dd932025080b7e4caddec3ed51725
Oluwagbemiga <taiwoemmanuel15@gmail.com>
2025-11-02 06:22:15 +0000

    Update requirements.txt with the needed requirements

--- api/views.py
+++ api/views.py
@@ -5,4 +5,11 @@ from rest_framework.response import Response
 class HomeAPIView(APIView):
 
     def post(self, request, *args, **kwargs):
-        return Response("Welcome")
+        return Response(
+            {
+                "a" : "Welcome",
+                "b" : request.data,
+                "c" : request.query_params,
+            }
+            
+            )
